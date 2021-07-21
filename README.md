4e9ce39c13ad818ffadd6c4f24fdea93470f5aff
Nicolas SANS <nicolas.sansd@gmail.com>
2021-07-21 15:49:19 +0200

    parse url to create android uri

---
+++ README.md
@@ -0,0 +1,7 @@
+# Epitech Intra Chrome Extension
+
+## How to use
+
+```text
+> git clone https://github.com/frnikho/EpitechIntraChromExtension.git 
+```
--- app.js
+++ app.js
@@ -7,10 +7,16 @@ function main() {
     }
 
     chrome.tabs.query(query, (tabs) => {
+        let toRemove = ["/netsoul", "https://", "www.", "http://", "intra.epitech.eu", "/#!", "/#", "/all", "/group", "/soutenance"];
         let tab = tabs[0];
+        let url = tab.url;
+
+        for (let i = 0; i < toRemove.length; i++) {
+            url = url.replace(toRemove[i], '');
+        }
 
         new QRCode(document.getElementById("qrcode"), {
-            text: tab.url,
+            text: "intra://" + url,
             width: 256,
             height: 256,
             colorDark : "#000000",
